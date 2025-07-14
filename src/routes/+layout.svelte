b11cfdd1b29f92a9c21f5983b8e6d241a5d96b89
Khotso-Bore <u19180642@tuks.co.za>
2025-07-14 20:58:46 +0200

    vercel analytis

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
 	import '../app.css';
 	import Header from '../components/Header.svelte';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit'
 
 	let { children } = $props();
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 <Header />
