6cdf813433f737aa47cc0bf034df03ea63d94f7e
Khotso-Bore <u19180642@tuks.co.za>
2025-07-30 21:30:43 +0200

    title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,5 +8,13 @@
 	injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
+<svelte:head>
+	<title>Gaubus Schedule</title>
+	<link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="48x48" href="/favicon_io/favicon.ico">
+	<link rel="manifest" href="/favicon_io/site.webmanifest">
+	<meta name="og:title" content="Gaubus Schedule" />
+	<meta name="description" content="This application tracks the gaubus (gautrain bus) and shows the schedule for the selected route." />
+</svelte:head>
 <Header />
 {@render children()}
