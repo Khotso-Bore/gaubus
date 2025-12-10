48dfb49fa5ca65b8535cb96f417eae7452215c1c
Khotso-Bore <u19180642@tuks.co.za>
2025-12-10 19:00:22 +0200

    chnaged Downloads to Bus Routes Downloads

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,8 +1,16 @@
 <script>
-    // No script needed for simple links
+	// No script needed for simple links
 </script>
 
-<div class="bg-white px-8 py-4 flex items-center shadow-md">
-    <a href="/" class="text-[#2563eb] font-semibold text-lg mr-6 hover:text-[#1e40af] hover:underline transition-colors duration-200">Home</a>
-    <a href="/downloads" class="text-[#2563eb] font-semibold text-lg hover:text-[#1e40af] hover:underline transition-colors duration-200">Downloads</a>
+<div class="flex items-center bg-white px-8 py-4 shadow-md">
+	<a
+		href="/"
+		class="mr-6 text-lg font-semibold text-[#2563eb] transition-colors duration-200 hover:text-[#1e40af] hover:underline"
+		>Home</a
+	>
+	<a
+		href="/downloads"
+		class="text-lg font-semibold text-[#2563eb] transition-colors duration-200 hover:text-[#1e40af] hover:underline"
+		>Bus Routes Downloads</a
+	>
 </div>
